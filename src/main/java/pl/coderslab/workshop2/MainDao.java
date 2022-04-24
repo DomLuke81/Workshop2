980630297834649d97e3fd21b34ac1e65612f311
Dominik81 <d.kochanski.onoszko@gmail.com>
2022-04-24 23:11:22 +0200

    ver. 0.2 - input into

--- src/main/java/pl/coderslab/workshop2/MainDao.java
+++ src/main/java/pl/coderslab/workshop2/MainDao.java
@@ -1,2 +1,16 @@
-package pl.coderslab.workshop2;public class MainDao {
+package pl.coderslab.workshop2;
+
+import pl.coderslab.workshop2.entity.User;
+import pl.coderslab.workshop2.entity.UserDao;
+
+public class MainDao {
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+        User user = new User();
+        user.setUserName("Piotr");
+        user.setEmail("peter@yahoo.com");
+        user.setPassword("password");
+        user = userDao.create(user);
+        System.out.println(user.getId());
+    }
 }
--- src/main/java/pl/coderslab/workshop2/entity/UserDao.java
+++ src/main/java/pl/coderslab/workshop2/entity/UserDao.java
@@ -1,2 +1,42 @@
-package pl.coderslab.workshop2.entity;public class UserDao {
+package pl.coderslab.workshop2.entity;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.workshop2.DbUtil;
+
+import java.sql.*;
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    private static final String SELECT_USER_QUERY =
+            "SELECT ? FROM users";
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET ? = ? WHERE id = ?";
+    private static final String DELETE_USER_QUERY =
+            "DELETE FROM users WHERE id = ?";
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
